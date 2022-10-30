60d9fcdc15f8904ed4a7df85da1c4383148e37a8
SDdev2017 <sdoykin223@gmail.com>
2022-10-30 20:38:05 +0200

    added search feature

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -5,46 +5,39 @@ import { useFetchData } from '../hooks/useFetchData';
 import { format } from '../helpers'; 
 
 import { LoadingIcon } from './LoadingIcon';
+import { SearchContainer } from './SearchContainer';
 
 export const CountriesList:FC = () => {
   const {data, setRegion} = useFetchData();
-
-  const handleSelect = (e:React.ChangeEvent<HTMLSelectElement>) => {
-    setRegion(e.target.value);
-  }
+  const [searchInput, setSearchInput] = useState<string>('');
 
   return (
     <>
       <section className='search-container'>
-        <input type="search" name="countryname" aria-label="Search for a country" placeholder="Search for a country..."/>
-        <select aria-label='Filter by Region' defaultValue="europe" onChange={handleSelect}>
-          <option value="europe">Europe</option>
-          <option value="africa">Africa</option>
-          <option value="america">America</option>
-          <option value="asia">Asia</option>
-          <option value="oceania">Oceania</option>
-        </select>
+        <SearchContainer setRegion={setRegion} searchInput={searchInput} setSearchInput={setSearchInput} />
       </section>
 
       <section className='country-list-container'>
-        {data ? data.map((v:any) => {
-          const {name:{common}, flags:{svg}, population, region, capital} = v;
-          
-          return (
-          <article key={common}>
-            <Link to='details' state={{name: "Stefan", age: 21, gender: "Male"}} className='link'></Link>
-            <div className='image-container'>
-              <img src={svg} alt={`${common}'s flag`} />
-            </div>
-            <div className='country-information'>
-              <h3>{common}</h3>
-              <ul>
-                <li><strong>Population:</strong> {format(population)}</li>
-                <li><strong>Region:</strong> {region}</li>
-                <li><strong>Capital:</strong> {capital}</li>
-              </ul>
-            </div>
-          </article>)}) : <LoadingIcon/>
+        {data ? data
+          .filter((v:any) => v.name.common.toLowerCase().includes(searchInput.toLowerCase()))
+          .map((v:any) => {
+            const {name:{common}, flags:{svg}, population, region, capital} = v;
+            
+            return (
+            <article key={common}>
+              <Link to='details' state={v} className='link'></Link>
+              <div className='image-container'>
+                <img src={svg} alt={`${common}'s flag`} />
+              </div>
+              <div className='country-information'>
+                <h3>{common}</h3>
+                <ul>
+                  <li><strong>Population:</strong> {format(population)}</li>
+                  <li><strong>Region:</strong> {region}</li>
+                  <li><strong>Capital:</strong> {capital}</li>
+                </ul>
+              </div>
+            </article>)}) : <LoadingIcon/>
         }
       </section>
     </>
---
+++ src/components/SearchContainer.tsx
@@ -0,0 +1,39 @@
+import {FC} from 'react';
+
+interface Props {
+    searchInput: string,
+    setSearchInput: React.Dispatch<React.SetStateAction<string>>,
+    setRegion: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const SearchContainer:FC<Props> = ({searchInput, setSearchInput, setRegion}) => {
+    const handleSelect = (e:React.ChangeEvent<HTMLSelectElement>) => {
+        setRegion(e.target.value);
+        setSearchInput('');
+    }
+    
+    const handleSearchInput = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(e.target.value);
+    }
+
+    return (
+        <>
+            <input 
+                type="search" 
+                name="countryname" 
+                value={searchInput} 
+                aria-label="Search for a country" 
+                placeholder="Search for a country..." 
+                onChange={handleSearchInput}
+            />
+            
+            <select aria-label='Filter by Region' defaultValue="europe" onChange={handleSelect}>
+                <option value="europe">Europe</option>
+                <option value="africa">Africa</option>
+                <option value="america">America</option>
+                <option value="asia">Asia</option>
+                <option value="oceania">Oceania</option>
+            </select>
+        </>
+    );
+};
